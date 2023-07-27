d12146e6ce0c23052e035d65ff5abd30bc8f3e79
guocong <guocong199708@163.com>
2023-07-27 17:57:01 +0800

    main函数添加代码调整，第9集58分

--- example/basic_operation.go
+++ example/basic_operation.go
@@ -13,13 +13,20 @@ func main() {
 		panic(err)
 	}
 
-	db.Put([]byte("name"), []byte("bitcask"))
+	db.Put([]byte("name"), []byte("bitcask111"))
 	if err != nil {
 		panic(err)
 	}
+
 	val, err := db.Get([]byte("name"))
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println("val = ", string(val))
+
+	//err = db.Delete([]byte("name"))
+	//if err != nil {
+	//	panic(err)
+	//}
 }
