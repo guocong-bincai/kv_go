7d7de54df9560f28fcd1a71b53f6d19d7a57d87d
guocong <guocong199708@163.com>
2023-08-02 15:47:52 +0800

    第十集，阶段性提交

--- index/btree.go
+++ index/btree.go
@@ -1,8 +1,10 @@
 package index
 
 import (
+	"bytes"
 	"github.com/google/btree"
 	"kv-go/data"
+	"sort"
 	"sync"
 )
 
@@ -50,4 +52,79 @@ func (bt *BTree) Delete(key []byte) (*data.LogRecordPos, bool) {
 		return nil, false
 	}
 	return oldItem.(*Item).pos, true
+}
+
+// BTree 索引迭代器
+type btreeIterator struct {
+	currIndex int     //当前遍历的下标位置
+	reverse   bool    //是否是反向遍历
+	values    []*Item //key+位置索引信息
+}
+
+func newBTreeIterator(tree *btree.BTree, reverse bool) *btreeIterator {
+	var idx int
+	values := make([]*Item, tree.Len())
+
+	//将所有的数据存储到数组中
+	saveValues := func(it btree.Item) bool {
+		values[idx] = it.(*Item)
+		idx++
+		return true
+	}
+	//是否需要反向操作
+	if reverse {
+		tree.Descend(saveValues)
+	} else {
+		tree.Ascend(saveValues)
+	}
+
+	return &btreeIterator{
+		currIndex: 0,
+		reverse:   reverse,
+		values:    values,
+	}
+}
+
+func (bti *btreeIterator) Rewind(key []byte) {
+	bti.currIndex = 0
+}
+
+// Seek 根据穿日的key 查找到第一个大于（或小于）等于的目标key，根据从这个key开始遍历
+func (bti *btreeIterator) Seek(key []byte) {
+	//还是根据reverse去判断顺序，正序或者反序
+	if bti.reverse {
+		bti.currIndex = sort.Search(len(bti.values), func(i int) bool {
+			return bytes.Compare(bti.values[i].key, key) <= 0
+		})
+	} else {
+		bti.currIndex = sort.Search(len(bti.values), func(i int) bool {
+			return bytes.Compare(bti.values[i].key, key) >= 0
+		})
+	}
+}
+
+// Next 跳转到下一个key
+func (bti *btreeIterator) Next() {
+	bti.currIndex += 1
+}
+
+// Valid 是否有效，即是否已经遍历完所有的key，用于退出遍历
+func (bti *btreeIterator) Valid() bool {
+	return bti.currIndex < len(bti.values)
+}
+
+// Key 当前遍历位置的key 数据
+func (bti *btreeIterator) Key() []byte {
+	//返回当前位置对应的数据
+	return bti.values[bti.currIndex].key
+}
+
+// Value 当前遍历位置的Value 数据
+func (bti *btreeIterator) Value() *data.LogRecordPos {
+	return bti.values[bti.currIndex].pos
+}
+
+// Close 关闭迭代器，释放相应资源
+func (bti *btreeIterator) Close() {
+	bti.values = nil
 }
--- index/index.go
+++ index/index.go
@@ -52,3 +52,21 @@ type Item struct {
 func (ai *Item) Less(bi btree.Item) bool {
 	return bytes.Compare(ai.key, bi.(*Item).key) == -1
 }
+
+// Iterator 通用索引迭代器
+type Iterator interface {
+	//Rewind 重新回到迭代器的起点，即第一个数据
+	Rewind()
+	//Seek 根据传入的key 查找到第一个大于（或小于）等于的目标key
+	Seek(key []byte)
+	//Next 跳转到下一个key
+	Next()
+	//Valid 是否有效，即是否已经遍历完了所有的key 用于退出遍历
+	Valid() bool
+	//Key 当前遍历位置的key数据
+	Key() []byte
+	//Value 当前遍历位置的Value数据
+	Value() *data.LogRecordPos
+	//Close 关闭迭代器，释放相应资源
+	Close()
+}
